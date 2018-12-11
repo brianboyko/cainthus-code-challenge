39d8cbd5bdede8a3b8fde2af0250953e7e3aa8b6
Brian Boyko <brian.boyko@gmail.com>
2018-12-11 17:13:34 +0000

    Finally nailed that nasty bug and got sass working

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -165,6 +165,20 @@ module.exports = {
               },
             ],
           },
+          {
+            test: /\.(scss|sass)$/,
+            use: [
+              {
+                loader: "style-loader" // creates style nodes from JS strings
+              },
+              {
+                loader: "css-loader" // translates CSS into CommonJS
+              },
+              {
+                loader: "sass-loader" // compiles Sass to CSS
+              }
+            ]
+          },
           // "postcss" loader applies autoprefixer to our CSS.
           // "css" loader resolves paths in CSS and adds assets as dependencies.
           // "style" loader turns CSS into JS modules that inject <style> tags.
