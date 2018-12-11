fa83f043525a483bcbbd51ed278890be9740a594
Brian Boyko <brian.boyko@gmail.com>
2018-12-11 18:06:43 +0000

    Safety-commit-styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,21 @@
 import * as React from "react";
+import ReactSVG from "react-svg";
+import photoSrc from "../../static/img/camera-retro.svg";
 import "./header.sass";
 
 const Header = (props: any) => (
   <div className="header">
+    <div className="logo">
+      <ReactSVG svgClassName="logo__svg" src={photoSrc} />
+    </div>
     <div className="logo-type">SapientPhoto</div>
+    <div className="header__search-form">
+      <input className="header__search-form__input" />
+      <button className="header__search-form__button">
+        Search
+      </button>
+    </div>
   </div>
 );
 
-
-export default Header
+export default Header;
