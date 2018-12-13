608561b457b11ba8b98051f6cbc00c625c968b83
Brian Boyko <brian.boyko@gmail.com>
2018-12-13 00:51:59 +0000

    Added final data to webpack production config so that it gets correct environment variables

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -14,6 +14,7 @@ const paths = require("./paths");
 const getClientEnvironment = require("./env");
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const dotEnv = require("dotenv-webpack");
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
@@ -249,6 +250,10 @@ module.exports = {
     ]
   },
   plugins: [
+    new dotEnv({
+      path: ".env",
+      safe: ".env"
+    }),
     // Makes some environment variables available in index.html.
     // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
     // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
